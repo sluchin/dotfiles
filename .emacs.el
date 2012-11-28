ad7c3c9fdd283e89e896ea921fe18ed669287475
Tetsuya Higashi <berio.sequenza@gmail.com>
2012-11-28 14:16:34 +0900

    first commit

---
+++ .emacs.el
@@ -0,0 +1,396 @@
+;;; -*- mode: emacs-lisp; coding: utf-8; indent-tabs-mode: nil -*-
+;;; Emacs 初期化ファイル
+
+;;; バージョン
+;; GNU Emacs 23.3.1 (i686-pc-linux-gnu, GTK+ Version 2.24.10)
+;;  of 2012-09-22 on akateko, modified by Debian
+;;  Copyright (C) 2011 Free Software Foundation, Inc.
+
+;;; ウィンドウのサイズと色は起動オプションで指定する
+;; /usr/bin/emacs23 -rv -g 100x50-100+0
+
+;;; Windows の場合, 以下の設定をすること
+;; Cygwin の Base をインストールしパスを通す
+;; 環境変数 HOME を任意のディレクトリに設定する
+
+;;; 起動時間を高速にする
+(modify-frame-parameters nil '((wait-for-wm . nil)))
+
+;;; ロードパスの設定
+;; lisp の置き場所をここに追加
+(setq load-path
+      (append '(
+                "~/.emacs.d"
+                "~/.emacs.d/howm"
+                "~/.emacs.d/navi2ch"
+                "~/.emacs.d/egg"
+                "~/.emacs.d/magit"
+                "~/.emacs.d/auto-install"
+                ) load-path))
+
+;;; 日本語の info のバスを設定
+;; wget -O- http://www.rubyist.net/~rubikitch/archive/emacs-elisp-info-ja.tgz | tar xvfz -
+;; 目次ファイルに以下を追加 (/usr/share/info/dir)
+;; * Elisp-ja: (elisp-ja) Emacs Lisp Reference Manual(Japanese).
+;; * Emacs-ja: (emacs-ja) The extensible self-documenting tgext editor(Japanese).
+(when (file-directory-p "~/info")
+  (eval-and-compile (require 'info))
+  (add-to-list 'Info-directory-list "~/info"))
+
+;;; 初期画面を表示しない
+(setq inhibit-startup-screen t)
+
+;;; フォントの設定
+;; Linux と Windows で変えたほうが見やすい
+;; Osaka フォントなど見やすいがカラムがずれるのでやめた
+;; 使えるフォントを調べるには以下を評価する
+;; (prin1 (font-family-list))
+(when window-system
+  (cond ((eq system-type 'windows-nt)
+         ;; Windowsの場合
+         (set-face-attribute 'default nil
+                             :family "Lucida Console"
+                             :height 80)
+         (set-fontset-font nil 'japanese-jisx0208
+                           (font-spec :family "Hiragino Mincho Pro")))
+        (t
+         ;; それ以外
+         (set-face-attribute 'default nil
+                             :family "Monospace"
+                             :height 80)
+         (set-fontset-font nil 'japanese-jisx0208
+                           (font-spec :family "Hiragino Mincho Pro")))))
+
+;;; 色をつける
+(global-font-lock-mode t)
+
+;;; タイトルバーにフルパス名を表示
+(when window-system
+  (setq frame-title-format "%f"))
+
+;;; モードラインの表示
+(display-time)           ; 時間
+(line-number-mode t)     ; 行数
+(column-number-mode t)   ; カラム数
+(size-indication-mode t) ; ファイルサイズ
+(which-function-mode t)  ; 関数名
+
+;;; find-fileのデフォルト
+(cd "~/")
+
+;;; 検索時大文字小文字の区別
+(setq case-fold-search nil)
+
+;;; 釣り合いのとれる括弧をハイライトにする
+(show-paren-mode t)
+
+;;; 改行と同時にインデントも行う
+(define-key global-map (kbd "C-m") 'newline-and-indent)
+
+;;; 画像ファイルを表示する
+(auto-image-file-mode t)
+
+;;; 最近使ったファイルを保存
+(recentf-mode)
+
+;;; クリップボードと同期をとる
+;; (setq x-select-enable-clipboard t)
+
+;;; タブの設定
+(setq-default tab-width 4)
+
+;;; タブをスペースにする
+(setq-default indent-tabs-mode nil)
+
+;;; c言語インデント
+(defconst stroustrup-style
+  '((c-basic-offset . 4)
+    (c-comment-only-line-offset . 0)
+    (c-offsets-alist
+     (statement-block-intro . +)
+     (substatement-open . 0)
+     (label . 0)
+     (statement-cont . +)
+     (inline-open . nil))))
+
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+             (c-add-style "stroustrup-style" stroustrup-style t)
+             (c-set-style "stroustrup-style")))
+
+;;; ブラウザでURLを開く
+(defun browse-url-at-point ()
+  (interactive)
+  (let ((url-region (bounds-of-thing-at-point 'url)))
+    (when url-region
+      (browse-url (buffer-substring-no-properties (car url-region)
+                                                  (cdr url-region))))))
+(define-key global-map (kbd "C-c C-o") 'browse-url-at-point)
+
+;;; キーバインド
+;; find-function のキー割り当て
+;; C-x F 関数, C-x V 変数, C-x K キー割り当てコマンド
+(find-function-setup-keys)
+;; 改行・タブ・スペースを色づけする
+(define-key global-map (kbd "C-^") 'global-whitespace-mode)
+;; クリップボードにコピー
+(define-key global-map (kbd "C-<insert>") 'clipboard-kill-ring-save)
+;; クリップボードに切り取り
+(define-key global-map (kbd "S-DEL") 'clipboard-kill-region)
+;; クリップボードに貼り付け
+(define-key global-map (kbd "S-<insert>") 'clipboard-yank)
+
+;;; ここから標準 lisp (emacs23 以降) の設定
+;;; 行番号表示
+(when (eval-and-compile (require 'linum nil t)) ; 画面左に行数を表示する
+  (global-linum-mode t)                         ; デフォルトで linum-mode を有効にする
+  (setq linum-format "%5d"))                    ; 5桁分の領域を確保して行番号のあとにスペースを入れる
+
+;;; ファイラ(dired)
+(when (eval-and-compile (require 'wdired nil t))
+  ;; 編集可能にする
+  (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode))
+
+;;; 関数のアウトライン表示
+(when (eval-when-compile (require 'speedbar nil t))
+  (setq speedbar-use-images nil)
+  (setq speedbar-frame-parameters '((minibuffer)
+                                    (width . 50)
+                                    (border-width . 0)
+                                    (menu-bar-lines . 0)
+                                    (tool-bar-lines . 0)
+                                    (unsplittable . t)
+                                    (left-fringe . 0)))
+  (setq speedbar-hide-button-brackets-flag t)
+  (setq speedbar-tag-hierarchy-method '(speedbar-simple-group-tag-hierarchy) )
+  ;; 拡張子の追加
+  (add-hook 'speedbar-mode-hook
+            '(lambda ()
+               (speedbar-add-supported-extension '("js" "as" "html" "css" "php"
+                                                   "rst" "howm" "org" "ml" "scala" "*")))))
+;; 行番号を表示しない
+(defadvice linum-on(around my-linum-speedbar-on() activate)
+  (unless (eq major-mode 'speedbar-mode) ad-do-it))
+
+(define-key global-map (kbd "<f6>") 'speedbar)
+
+;;; Ediff Control Panel 専用のフレームを作成しない
+;; Windows の場合, 環境変数 CYGWIN に "nodosfilewarning" を設定する
+(eval-when-compile (require 'ediff))
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;;; バッファの切り替えをインクリメンタルにする
+(when (eval-and-compile (require 'iswitchb nil t))
+  (iswitchb-mode t)
+  (setq read-buffer-function 'iswitchb-read-buffer)
+  (setq iswitchb-regexp nil)
+  (setq iswitchb-prompt-newbuffer nil))
+
+;;; 文書作成 (org-mode)
+(defvar org-code-reading-software-name nil)
+(defvar org-code-reading-file "code-reading.org")
+(defun org-code-reading-read-software-name ()
+  (set (make-local-variable 'org-code-reading-software-name)
+       (read-string "Code Reading Software: "
+                    (or org-code-reading-software-name
+                        (file-name-nondirectory
+                         (buffer-file-name))))))
+(defun org-code-reading-get-prefix (lang)
+  (concat "[" lang "]"
+          "[" (org-code-reading-read-software-name) "]"))
+
+(when (and (eval-when-compile (require 'org nil t))
+           (eval-when-compile (require 'org-remember nil t))
+           (require 'org-install nil t))
+
+  ;; org-mode での強調表示を可能にする
+  (add-hook 'org-mode-hook 'turn-on-font-lock)
+  ;; 見出しの余分な*を消す
+  (setq org-hide-leading-stars t)
+  ;; org-remember のディレクトリ
+  (setq org-directory "~/memo/")
+  ;; org-remember のファイル名
+  (setq org-default-notes-file (concat org-directory "agenda.org"))
+  (setq org-startup-truncated nil)
+  (setq org-return-follows-link t)
+  (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+  (org-remember-insinuate)
+  (setq org-remember-templates
+        '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
+          ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
+          ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")))
+
+  ;; ソースコードを読みメモする
+  (defun org-remember-code-reading ()
+    (interactive)
+    (let* ((prefix (org-code-reading-get-prefix (substring (symbol-name major-mode) 0 -5)))
+           (org-remember-templates
+            `(("CodeReading" ?r "** %(identity prefix)%?\n   \n   %a\n   %t"
+               ,org-code-reading-file "Memo"))))
+      (org-remember)))
+  ;; キーバインド
+  (define-key global-map (kbd "C-c l") 'org-store-link)
+  (define-key global-map (kbd "C-c a") 'org-agenda)
+  (define-key global-map (kbd "C-c r") 'org-remember)
+  (define-key global-map (kbd "C-c c") 'org-remember-code-reading)
+  (define-key global-map (kbd "C-c b") 'org-iswitchb))
+
+;;; ここまで標準 lisp
+
+;;; ここから拡張 lisp の設定
+;; 使用する場合 lisp をロードパスの通ったところにインストールすること
+;;; インストーラ
+;; wget http://www.emacswiki.org/emacs/download/auto-install.el
+;; Windowsはwgetが必要なのでとりあえず使わない
+(unless (eq system-type 'windows-nt)
+  (when (eval-and-compile (require 'auto-install nil t))
+    (auto-install-update-emacswiki-package-name t)
+    (auto-install-compatibility-setup)))
+
+;;; リドゥ
+;; M-x install-elisp-from-emacswiki redo+.el
+(when (require 'redo+ nil t)
+  (define-key global-map (kbd "C-.") 'redo))
+
+;;; 行番号表示する必要のないモードでは表示しない
+;; M-x install-elisp-from-emacswiki linum-off.el
+(require 'linum-off nil t)
+
+;;; 2chビューア(navi2ch)
+;; wget -O- http://sourceforge.net/projects/navi2ch/files/navi2ch/navi2ch-1.8.4/navi2ch-1.8.4.tar.gz/download | tar xvfz -
+(when (locate-library "navi2ch")
+  (autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t))
+
+;;; メモ(howm)
+;; wget -O- http://howm.sourceforge.jp/a/howm-1.4.0.tar.gz | tar xvfz -
+(when (eval-when-compile (require 'howm nil t))
+  (setq howm-menu-lang 'ja)
+  ;; デュアルブートでLinuxとWindowsで共有するための設定をする
+  (cond ((and (eq system-type 'gnu/linux)
+         (file-directory-p "/dos"))
+         (setq howm-directory "/dos/howm"))
+        ((and (eq system-type 'windows-nt)
+         (file-directory-p "e:"))
+          (setq howm-directory "e:/howm"))
+        (t
+          (setq howm-directory "~/howm")))
+  ;; 除外するファイル
+  (setq howm-excluded-file-regexp
+        "\\(^\\|/\\)\\([.]\\|\\(menu\\(_edit\\)?\\|0+-0+-0+\\)\\)\\|[~#]$\\|\\.bak$\\|/CVS/")
+  (define-key global-map (kbd "C-c , ,") 'howm-menu)
+  (autoload 'howm-menu "howm-mode" "Hitori Otegaru Wiki Modoki" t))
+
+;;; GNU Global
+;; apt-get install global
+(when (and (executable-find "global") (locate-library "gtags"))
+  (autoload 'gtags-mode "gtags" nil t)
+  (add-hook 'c-mode-hook 'gtags-mode)
+  (add-hook 'c++-mode-hook 'gtags-mode)
+  (add-hook 'java-mode-hook 'gtags-mode)
+  (define-key global-map (kbd "<f3>") 'gtags-find-with-grep))
+
+;;; grepの色
+;; M-x install-elisp http://www.bookshelf.jp/elc/color-grep.el
+(eval-and-compile (require 'color-grep nil t))
+
+;;; 日本語入力 (ddskk)
+;; sudo apt-get install ddskk
+(when (eval-and-compile (require 'skk nil t))
+  (define-key global-map (kbd "C-x C-j") 'skk-mode))
+
+;;; 試行錯誤用ファイル
+;; M-x install-elisp-from-emacswiki open-junk-file.el
+(when (eval-and-compile (require 'open-junk-file nil t))
+  ;; C-x C-z で試行錯誤用ファイルを開く
+  (define-key global-map (kbd "C-x C-z") 'open-junk-file))
+
+;;; 式の評価結果を注釈するための設定
+;; M-x install-elisp-from-emacswiki lispxmp.el
+(when (eval-and-compile (require 'lispxmp nil t))
+  ;; C-c C-d で注釈
+  (define-key emacs-lisp-mode-map (kbd "C-c C-d") 'lispxmp))
+
+;;; 括弧の対応を保持して編集する設定
+;; M-x install-elisp http://mumble.net/~campbell/emacs/paredit.el
+;; scrachバッファでこれが有効になっていると評価できないので注意
+(when (eval-and-compile (require 'paredit nil t))
+  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook  'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook  'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook  'emacs-lisp-mode-hook))
+
+;;; 自動バイトコンパイル
+;; M-x install-elisp-from-emacswiki auto-async-byte-compile.el
+(when (eval-and-compile (require 'auto-async-byte-compile nil t))
+  ;; バイトコンパイルしないファイル
+  (setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+  (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode))
+
+;;; ミニバッファに関数のhelp表示
+;; M-x install-elisp-from-emacswiki eldoc-extension.el
+(when (eval-when-compile (require 'eldoc-extension nil t))
+  (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+  (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+  (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+  (setq eldoc-idle-delay 0.2)
+  ;; モードラインに ElDoc と表示しない
+  (setq eldoc-minor-mode-string ""))
+
+;;; *Help* にメモを書き込む
+;; M-x install-elisp-from-emacswiki usage-memo.el
+(when (eval-and-compile (require 'usage-memo nil t))
+  (setq umemo-base-directory "~/.emacs.d/umemo")
+  (umemo-initialize))
+
+;;; git の設定
+;; git clone git://github.com/byplayer/egg.git
+;; とりあえず, Windowsでは使わない
+(unless (eq system-type 'windows-nt)
+  (when (and (executable-find "git") (locate-library "egg"))
+    (require 'egg nil t)))
+
+;;; git の設定
+;; git clone git://github.com/jdhuntington/magit.git
+;; とりあえず, Windowsでは使わない
+(unless (eq system-type 'windows-nt)
+  (when (and (executable-find "git") (locate-library "magit"))
+    (require 'magit nil t)))
+
+;;; Windows の設定
+(when (eq system-type 'windows-nt)
+  ;; Windows のショートカットをリンクできるようにする
+  ;; http://centaur.maths.qmw.ac.uk/Emacs/files/w32-symlinks.el
+  (when (eval-and-compile (require 'w32-symlinks nil t))
+    (custom-set-variables '(w32-symlinks-handle-shortcuts t))
+    ;; NTEmacs で動かすための設定
+    (defadvice insert-file-contents-literally
+      (before insert-file-contents-literally-before activate)
+      (set-buffer-multibyte nil))
+
+    (defadvice minibuffer-complete (before expand-symlinks activate)
+      (let ((file (expand-file-name
+                   (buffer-substring-no-properties
+                    (line-beginning-position) (line-end-position)))))
+        (when (file-symlink-p file)
+          (delete-region (line-beginning-position) (line-end-position))
+          (insert (w32-symlinks-parse-symlink file))))))
+
+  ;; dired で Windows に関連付けられたアプリを起動する
+  ;; http://www.emacswiki.org/emacs/download/w32-shell-execute.el
+  (when (eval-when-compile (require 'w32-shell-execute nil t))
+    (defun uenox-dired-winstart ()
+      "Type '[uenox-dired-winstart]': win-start the current line's file."
+      (interactive)
+      (if (eq major-mode 'dired-mode)
+          (let ((fname (dired-get-filename)))
+            (w32-shell-execute "open" fname)
+            (message "win-started %s" fname))))
+    ;; dired のキー割り当て追加
+    (add-hook 'dired-mode-hook
+              (lambda ()
+                (define-key dired-mode-map "z" 'uenox-dired-winstart))))
+
+  ;; find や grep で"grep: NUL: No such file or directory" を回避する
+  (setq null-device "/dev/null"))
+
+;;; ここまで拡張 lisp
