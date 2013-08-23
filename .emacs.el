353d8bbda8ff61736ff361c5892d4f891e36e829
Tetsuya Higashi <higashit@itec-hokkaido.co.jp>
2013-08-23 23:18:09 +0900

    modified undohist for windows

--- .emacs.el
+++ .emacs.el
@@ -2584,6 +2584,26 @@
 ;;; アンドゥ履歴
 ;; (install-elisp "http://cx4a.org/pub/undohist.el")
 (when (eval-and-compile (require 'undohist nil t))
+  (when (eq system-type 'windows-nt)
+    (defun make-undohist-file-name (file)
+      (setq file (convert-standard-filename (expand-file-name file)))
+      (if (eq (aref file 1) ?:)
+          (setq file (concat "/"
+                             "drive_"
+                             (char-to-string (downcase (aref file 0)))
+                             (if (eq (aref file 2) ?/)
+                                 ""
+                               (if (eq (aref file 2) ?\\)
+                                   ""
+                                 "/"))
+                             (substring file 2))))
+      (setq file (expand-file-name
+                  (subst-char-in-string
+                   ?/ ?!
+                   (subst-char-in-string
+                    ?\\ ?!
+                    (replace-regexp-in-string "!" "!!"  file)))
+                  undohist-directory))))
   (when (fboundp 'undohist-initialize)
     (undohist-initialize)))
 
