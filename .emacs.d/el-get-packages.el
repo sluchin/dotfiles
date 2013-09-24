53219e36a04ff7df152c8ce95b0d62de52e3600e
Tetsuya Higashi <higashit@itec-hokkaido.co.jp>
2013-09-24 18:52:10 +0900

    added github url

--- .emacs.d/el-get-packages.el
+++ .emacs.d/el-get-packages.el
@@ -124,70 +124,87 @@
           (:name cp5022x
                  :type github
                  :description "cp50220, cp50221, cp50222 coding system."
-                 :pkgname "awasira/cp5022x.el")
+                 :pkgname "awasira/cp5022x.el"
+                 :url "git://github.com/awasira/cp5022x.el.git")
           (:name popup
                  :type github
                  :description "Visual popup interface."
-                 :pkgname "auto-complete/popup-el")
+                 :pkgname "auto-complete/popup-el"
+                 :url "git://github.com/auto-complete/popup-el.git")
           (:name bm
                  :type github
                  :description "Visible bookmarks in buffer."
-                 :pkgname "joodland/bm")
+                 :pkgname "joodland/bm"
+                 :url "git://github.com/joodland/bm.git")
           (:name magit
                  :type github
                  :description "control Git from Emacs."
-                 :pkgname "magit/magit")
+                 :pkgname "magit/magit"
+                 :url "git://github.com/magit/magit.git")
           (:name git-modes
                  :type github
                  :description "Emacs modes for various Git-related files."
-                 :pkgname "magit/git-modes")
+                 :pkgname "magit/git-modes"
+                 :url "git://github.com/magit/git-modes.git")
           (:name twittering-mode
                  :type github
                  :description "Major mode for Twitter."
-                 :pkgname "hayamiz/twittering-mode")
+                 :pkgname "hayamiz/twittering-mode"
+                 :url "git://github.com/hayamiz/twittering-mode.git")
           (:name tomatinho
                  :type github
                  :description "Pomodoro timer."
-                 :pkgname "konr/tomatinho")
+                 :pkgname "konr/tomatinho"
+                 :url "git://github.com/konr/tomatinho.git")
           (:name yasnippet
                  :type github
                  :description "Yet another snippet extension for Emacs."
-                 :pkgname "capitaomorte/yasnippet")
+                 :pkgname "capitaomorte/yasnippet"
+                 :url "git://github.com/capitaomorte/yasnippet.git")
           (:name ac-slime
                  :type github
                  :description "An auto-complete source using slime completions."
-                 :pkgname "purcell/ac-slime")
+                 :pkgname "purcell/ac-slime"
+                 :url "git://github.com/purcell/ac-slime.git")
           (:name auto-complete-clang
                  :type github
                  :description "Auto Completion source for clang for GNU Emacs."
-                 :pkgname "brianjcj/auto-complete-clang")
+                 :pkgname "brianjcj/auto-complete-clang"
+                 :url "git://github.com/brianjcj/auto-complete-clang.git")
           (:name ajc-java-complete
                  :type github
                  :description "Auto Java Completion for GNU Emacs."
-                 :pkgname "jixiuf/ajc-java-complete")
+                 :pkgname "jixiuf/ajc-java-complete"
+                 :url "git://github.com/jixiuf/ajc-java-complete.git")
           (:name yasnippet-java-mode
                  :type github
                  :description "yasnippet java-mode."
-                 :pkgname "nekop/yasnippet-java-mode")
+                 :pkgname "nekop/yasnippet-java-mode"
+                 :url "git://github.com/nekop/yasnippet-java-mode.git")
           (:name malabar-mode
                  :type github
                  :description "A better Java mode for Emacs"
-                 :pkgname "espenhw/malabar-mode")
+                 :pkgname "espenhw/malabar-mode"
+                 :url "git://github.com/espenhw/malabar-mode.git")
           (:name haskell-mode
                  :type github
                  :description "A Haskell editing mode."
-                 :pkgname "haskell/haskell-mode")
+                 :pkgname "haskell/haskell-mode"
+                 :url "git://github.com/haskell/haskell-mode")
           (:name clojure-mode
                  :type github
                  :description "Major mode for Clojure code."
-                 :pkgname "jochu/clojure-mode")
+                 :pkgname "jochu/clojure-mode"
+                 :url "git://github.com/jochu/clojure-mode.git")
           (:name swank-clojure
                  :type github
                  :description "slime adapter for clojure."
-                 :pkgname "jochu/swank-clojure")
+                 :pkgname "jochu/swank-clojure"
+                 :url "git://github.com/jochu/swank-clojure.git")
           (:name navi2ch
                  :type github
                  :description "Navigator for 2ch for Emacsen."
                  :website "http://navi2ch.sourceforge.net/"
-                 :pkgname "naota/navi2ch"))))
+                 :pkgname "naota/navi2ch"
+                 :url "git://github.com/naota/navi2ch.git"))))
 
