d84a109d2a3bdb8aa4c856606c83ed8b93bea444
Tetsuya Higashi <berio.sequenza@gmail.com>
2014-09-14 12:37:45 +0900

    modified calendar and dired

---
+++ submodule_add.sh
@@ -0,0 +1,6 @@
+git submodule add https://github.com/robbyrussell/oh-my-zsh.git .zsh.d/oh-my-zsh
+git submodule add https://github.com/zsh-users/zaw.git .zsh.d/plugin/zaw
+git submodule add https://github.com/hchbaw/auto-fu.zsh.git .zsh.d/plugin/auto-fu
+git submodule add https://github.com/marzocchi/zsh-notify.git .zsh.d/plugin/zsh-notify
+git submodule add https://github.com/erikw/tmux-powerline.git .tmux/tmux-powerline
+git submodule add https://github.com/thewtex/tmux-mem-cpu-load.git .tmux/tmux-mem-cpu-load
---
+++ submodule_deinit.sh
@@ -0,0 +1,6 @@
+git submodule deinit .zsh.d/oh-my-zsh
+git submodule deinit .zsh.d/plugin/zaw
+git submodule deinit .zsh.d/plugin/auto-fu
+git submodule deinit .zsh.d/plugin/zsh-notify
+git submodule deinit .tmux/tmux-powerline
+git submodule deinit .tmux/tmux-mem-cpu-load
