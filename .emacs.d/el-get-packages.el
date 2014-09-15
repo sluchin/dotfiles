00c9d7d20d8e486305f675fc351f304d9a7776b6
Tetsuya Higashi <berio.sequenza@gmail.com>
2014-09-15 23:09:15 +0900

    modified el-get url

--- .emacs.d/el-get-packages.el
+++ .emacs.d/el-get-packages.el
@@ -40,13 +40,13 @@
         perltidy
         php-completion
         xml-parse
+        csv-mode
         sl
         ;; http
         gtk-look
         iman
         completing-help
         undohist
-        csv-mode
         minibuf-isearch
         paredit
         pomodoro
@@ -130,6 +130,7 @@
          (:name perltidy :type emacswiki)
          (:name php-completion :type emacswiki)
          (:name xml-parse :type emacswiki)
+         (:name csv-mode :type emacswiki)
          (:name sl :type emacswiki)
          ;; http
          (:name gtk-look
@@ -148,10 +149,6 @@
                 :type http
                 :description "Record and recover undo history."
                 :url "http://cx4a.org/pub/undohist.el")
-         (:name csv-mode
-                :type http
-                :description "Major mode for editing comma/char separated values."
-                :url "http://bzr.savannah.gnu.org/lh/emacs/elpa/download/head:/csvmode.el-20120312160844-puljoum8kcsf2xcu-2/csv-mode.el")
          (:name minibuf-isearch
                 :type http
                 :description "incremental search on minibuffer history."
@@ -163,15 +160,15 @@
          (:name pomodoro
                 :type http
                 :description "Pomodoro Technique in Emacs."
-                :url "https://raw.github.com/syohex/emacs-utils/master/pomodoro.el")
+                :url "http://raw.github.com/syohex/emacs-utils/master/pomodoro.el")
          (:name tea-time
                 :type http
                 :description "Simple timer package, useful to make perfect tea."
-                :url "https://raw.github.com/krick/tea-time/master/tea-time.el")
+                :url "http://raw.github.com/krick/tea-time/master/tea-time.el")
          (:name package-install
                 :type http
                 :description "auto-installer for package.el."
-                :url "https://raw.github.com/gist/1884092/4542d018c14fb8fb9f2e6b1a69b01abb1ce475bb/package-install.el")
+                :url "http://repo.or.cz/w/emacs.git/blob_plain/HEAD:/lisp/emacs-lisp/package.el")
          (:name summarye
                 :type http
                 :description "list up matched strings from a buffer, and display them in summary buffer."
@@ -189,168 +186,168 @@
                 :type github
                 :description "cp50220, cp50221, cp50222 coding system."
                 :pkgname "awasira/cp5022x.el"
-                :url "git://github.com/awasira/cp5022x.el.git")
+                :url "https://github.com/awasira/cp5022x.el")
          (:name auto-complete
                 :type github
                 :description "Emacs auto-complete package. http://auto-complete.org."
                 :pkgname "auto-complete/auto-complete"
-                :url "git://github.com/auto-complete/auto-complete.git")
+                :url "https://github.com/auto-complete/auto-complete")
          (:name bm
                 :type github
                 :description "Visible bookmarks in buffer."
                 :pkgname "joodland/bm"
-                :url "git://github.com/joodland/bm.git")
+                :url "https://github.com/joodland/bm")
          (:name direx-el
                 :type github
                 :description "direx.el is a simple directory explorer."
                 :pkgname "m2ym/direx-el"
-                :url "git://github.com/m2ym/direx-el.git")
+                :url "https://github.com/m2ym/direx-el")
          (:name popwin-el
                 :type github
                 :description "Popup Window Manager for Emacs"
                 :pkgname "m2ym/popwin-el"
-                :url "git://github.com/m2ym/popwin-el.git")
+                :url "https://github.com/m2ym/popwin-el")
          (:name magit
                 :type github
                 :description "control Git from Emacs."
                 :pkgname "magit/magit"
-                :url "git://github.com/magit/magit.git")
+                :url "https://github.com/magit/magit")
          (:name git-modes
                 :type github
                 :description "Emacs modes for various Git-related files."
                 :pkgname "magit/git-modes"
-                :url "git://github.com/magit/git-modes.git")
+                :url "https://github.com/magit/git-modes")
          (:name twittering-mode
                 :type github
                 :description "Major mode for Twitter."
                 :pkgname "hayamiz/twittering-mode"
-                :url "git://github.com/hayamiz/twittering-mode.git")
+                :url "https://github.com/hayamiz/twittering-mode")
          (:name tomatinho
                 :type github
                 :description "Pomodoro timer."
                 :pkgname "konr/tomatinho"
-                :url "git://github.com/konr/tomatinho.git")
+                :url "https://github.com/konr/tomatinho")
          (:name yasnippet
                 :type github
                 :description "Yet another snippet extension for Emacs."
                 :pkgname "capitaomorte/yasnippet"
-                :url "git://github.com/capitaomorte/yasnippet.git")
+                :url "https://github.com/capitaomorte/yasnippet")
          (:name ac-slime
                 :type github
                 :description "An auto-complete source using slime completions."
                 :pkgname "purcell/ac-slime"
-                :url "git://github.com/purcell/ac-slime.git")
+                :url "https://github.com/purcell/ac-slime")
          (:name auto-complete-clang
                 :type github
                 :description "Auto Completion source for clang for GNU Emacs."
                 :pkgname "brianjcj/auto-complete-clang"
-                :url "git://github.com/brianjcj/auto-complete-clang.git")
+                :url "https://github.com/brianjcj/auto-complete-clang")
          (:name ajc-java-complete
                 :type github
                 :description "Auto Java Completion for GNU Emacs."
                 :pkgname "jixiuf/ajc-java-complete"
-                :url "git://github.com/jixiuf/ajc-java-complete.git")
+                :url "https://github.com/jixiuf/ajc-java-complete")
          (:name yasnippet-java-mode
                 :type github
                 :description "yasnippet java-mode."
                 :pkgname "nekop/yasnippet-java-mode"
-                :url "git://github.com/nekop/yasnippet-java-mode.git")
+                :url "https://github.com/nekop/yasnippet-java-mode")
          (:name php-eldoc
                 :type github
                 :description "eldoc-mode plugin for PHP source code"
                 :pkgname "zenozeng/php-eldoc"
-                :url "git://github.com/zenozeng/php-eldoc.git")
+                :url "https://github.com/zenozeng/php-eldoc")
          (:name css-eldoc
                 :type github
                 :description "eldoc-mode plugin for CSS"
                 :pkgname "zenozeng/css-eldoc"
-                :url "git://github.com/zenozeng/css-eldoc.git")
+                :url "https://github.com/zenozeng/css-eldoc")
          (:name php-mode
                 :type github
                 :description "PHP Mode for GNU Emacs."
                 :pkgname "ejmr/php-mode"
-                :url "git://github.com/ejmr/php-mode.git")
+                :url "https://github.com/ejmr/php-mode")
          (:name php-extras
                 :type github
                 :description "Extra features for Emacs `php-mode'."
                 :pkgname "arnested/php-extras"
-                :url "git://github.com/arnested/php-extras.git")
+                :url "https://github.com/arnested/php-extras")
          (:name phpplus-mode
                 :type github
                 :description "A better php mode for emacs, with specific support for Zend Framework 1."
                 :pkgname "echosa/phpplus-mode"
-                :url "git://github.com/echosa/phpplus-mode.git")
+                :url "https://github.com/echosa/phpplus-mode")
          (:name emacs-php-align
                 :type github
                 :description "Emacs's alignment configuration for PHP."
                 :pkgname "tetsujin/emacs-php-align"
-                :url "git://github.com/tetsujin/emacs-php-align.git")
+                :url "https://github.com/tetsujin/emacs-php-align")
          (:name inf-php
                 :type github
                 :description "Run a php interactive shell on emacs."
                 :pkgname "taksatou/inf-php"
-                :url "git://github.com/taksatou/inf-php.git")
+                :url "https://github.com/taksatou/inf-php")
          (:name yaml-mode
                 :type github
                 :description "The emacs major mode for editing files in the YAML data serialization format."
                 :pkgname "yoshiki/yaml-mode"
-                :url "git://github.com/yoshiki/yaml-mode.git")
+                :url "https://github.com/yoshiki/yaml-mode")
          (:name geben-on-emacs
                 :type github
                 :description "GEBEN is a software package that interfaces Emacs to DBGp protocol with which you can debug running scripts interactive."
                 :pkgname "mcorde/geben-on-emacs"
-                :url "git://github.com/mcorde/geben-on-emacs.git")
+                :url "https://github.com/mcorde/geben-on-emacs")
          (:name malabar-mode
                 :type github
                 :description "A better Java mode for Emacs"
                 :pkgname "espenhw/malabar-mode"
-                :url "git://github.com/espenhw/malabar-mode.git")
+                :url "https://github.com/espenhw/malabar-mode")
          (:name haskell-mode
                 :type github
                 :description "A Haskell editing mode."
                 :pkgname "haskell/haskell-mode"
-                :url "git://github.com/haskell/haskell-mode.git")
+                :url "https://github.com/haskell/haskell-mode")
          (:name clojure-mode
                 :type github
                 :description "Major mode for Clojure code."
                 :pkgname "jochu/clojure-mode"
-                :url "git://github.com/jochu/clojure-mode.git")
+                :url "https://github.com/jochu/clojure-mode")
          (:name swank-clojure
                 :type github
                 :description "slime adapter for clojure."
                 :pkgname "jochu/swank-clojure"
-                :url "git://github.com/jochu/swank-clojure.git")
+                :url "https://github.com/jochu/swank-clojure")
          (:name navi2ch
                 :type github
                 :description "Navigator for 2ch for Emacsen."
                 :website "http://navi2ch.sourceforge.net/"
                 :pkgname "naota/navi2ch"
-                :url "git://github.com/naota/navi2ch.git")
+                :url "https://github.com/naota/navi2ch")
          (:name web-mode
                 :type github
                 :description "web-mode.el is an emacs major mode for editing html templates."
                 :pkgname "fxbois/web-mode"
-                :url "git://github.com/fxbois/web-mode.git")
+                :url "https://github.com/fxbois/web-mode")
          (:name emacs-helm
                 :type github
                 :description "Emacs incremental completion and selection narrowing framework."
                 :pkgname "emacs-helm/helm"
-                :url "git://github.com/emacs-helm/helm.git")
+                :url "https://github.com/emacs-helm/helm")
          (:name helm-ls-git
                 :type github
                 :description "Yet another helm to list git file."
                 :pkgname "emacs-helm/helm-ls-git"
-                :url "git://github.com/emacs-helm/helm-ls-git.git")
+                :url "https://github.com/emacs-helm/helm-ls-git")
          (:name helm-descbings
                 :type github
                 :description "A helm frontend for describe-bindings."
                 :pkgname "emacs-helm/helm-descbinds"
-                :url "git://github.com/emacs-helm/helm-descbinds.git")
+                :url "https://github.com/emacs-helm/helm-descbinds")
          (:name org-mode
                 :type github
                 :description "Org mode is for keeping notes."
-                :url "git://orgmode.org/org-mode.git")
+                :url "https://github.com/jwiegley/org-mode")
          (:name japanese-holidays
                 :type github
                 :description "calendar functions for the Japanese calendar."
-                :url "git://github.com/emacs-jp/japanese-holidays.git"))))
+                :url "https://github.com/emacs-jp/japanese-holidays"))))
