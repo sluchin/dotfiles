c5be666c0868e3af87d1b1778e631d07d8ca7457
Tetsuya Higashi <berio.sequenza@gmail.com>
2017-12-04 01:55:11 +0900

    added annotations

--- .emacs.d/cpp-gray.el
+++ .emacs.d/cpp-gray.el
@@ -1,12 +1,14 @@
-(setq cpp-known-face 'default)
-(setq cpp-unknown-face 'default)
-(setq cpp-face-type 'dark)
-(setq cpp-known-writable 't)
-(setq cpp-unknown-writable 't)
-(setq cpp-edit-list
-      '(("1" nil
-         (background-color . "dim gray")
-         both nil)
-        ("0"
-         (background-color . "dim gray")
-         default both)))
+(defun cpp-gray ()
+  (interactive)
+  (setq cpp-known-face 'default)
+  (setq cpp-unknown-face 'default)
+  (setq cpp-face-type 'dark)
+  (setq cpp-known-writable 't)
+  (setq cpp-unknown-writable 't)
+  (setq cpp-edit-list
+        '(("1" nil
+           (background-color . "dim gray")
+           both nil)
+          ("0"
+           (background-color . "dim gray")
+           default both))))
--- .emacs.d/cpp-invisible.el
+++ .emacs.d/cpp-invisible.el
@@ -1,8 +1,10 @@
-(setq cpp-known-face 'invisible)
-(setq cpp-unknown-face 'invisible)
-(setq cpp-face-type 'dark)
-(setq cpp-known-writable 't)
-(setq cpp-unknown-writable 't)
-(setq cpp-edit-list
-      '(("1" nil invisible both nil)
-        ("0" invisible nil both nil)))
+(defun cpp-invisible ()
+  (interactive)
+  (setq cpp-known-face 'invisible)
+  (setq cpp-unknown-face 'invisible)
+  (setq cpp-face-type 'dark)
+  (setq cpp-known-writable 't)
+  (setq cpp-unknown-writable 't)
+  (setq cpp-edit-list
+        '(("1" nil invisible both nil)
+          ("0" invisible nil both nil))))
